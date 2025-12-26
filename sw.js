e2bdba784c4cb81b5e28a7f5a27c2c667cb1eff9
AbdMou191 <163216969+AbdMou191@users.noreply.github.com>
2025-12-26 13:27:22 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,26 @@
+const cacheName = 'math-kids-v1';
+const assets = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './manifest.json'
+];
+
+// تثبيت ملف الخدمة وتخزين الملفات في الذاكرة المؤقتة
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      cache.addAll(assets);
+    })
+  );
+});
+
+// تشغيل التطبيق من الذاكرة المؤقتة عند انقطاع الإنترنت
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
+});
